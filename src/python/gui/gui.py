6ce29de957bd8f8af938cac22fbfd35fb2e66911
Bob Cowdery <bob@bobcowdery.plus.com>
2019-04-25 15:41:05 +0100

    Developing.

--- src/python/gui/gui.py
+++ src/python/gui/gui.py
@@ -62,33 +62,65 @@ class AppWindow(QMainWindow):
     # Setup the UI
     def __setup_ui(self, grid):
        
-        self.__ip5v = QPushButton("IP 5v Switch")
-        self.__ip5v.clicked.connect(self.__ip5v_evnt)
-        grid.addWidget(self.__ip5v, 0, 0)
-        self.__setStyle(self.__ip5v)
+        ip5v_label = QLabel("IP 5v Switch")
+        ip5v_cb_on = QCheckBox("On")
+        ip5v_cb_off = QCheckBox("Off")
+        self.__ip5v_grp = QButtonGroup()
+        self.__ip5v_btn = QPushButton("Set")
+        self.__setup_func(grid, ip5v_label, ip5v_cb_on, ip5v_cb_off, self.__ip5v_grp, self.__ip5v_btn, self.__ip5v_evnt, 0)
         
-        self.__camera = QPushButton("Camera + Lights")
-        self.__camera.clicked.connect(self.__camera_evnt)
-        grid.addWidget(self.__camera, 1, 0)
-        self.__setStyle(self.__camera)
+        camera_label = QLabel("Camera")
+        camera_cb_on = QCheckBox("On")
+        camera_cb_off = QCheckBox("Off")
+        self.__camera_grp = QButtonGroup()
+        self.__camera_btn = QPushButton("Set")
+        self.__setup_func(grid, camera_label, camera_cb_on, camera_cb_off, self.__camera_grp, self.__camera_btn, self.__camera_evnt, 1)
         
-        self.__ant_sw = QPushButton("Antenna Switch")
-        self.__ant_sw.clicked.connect(self.__ant_sw_evnt)
-        grid.addWidget(self.__ant_sw, 2, 0)
-        self.__setStyle(self.__ant_sw)
+        ant_sw_label = QLabel("Antenna Switch")
+        ant_sw_cb_on = QCheckBox("On")
+        ant_sw_cb_off = QCheckBox("Off")
+        self.__ant_sw_grp = QButtonGroup()
+        self.__ant_sw_btn = QPushButton("Set")
+        self.__setup_func(grid, ant_sw_label, ant_sw_cb_on, ant_sw_cb_off, self.__ant_sw_grp, self.__ant_sw_btn, self.__ant_sw_evnt, 2)
         
-        self.__hpsdr = QPushButton("HPSDR")
-        self.__hpsdr.clicked.connect(self.__hpsdr_evnt)
-        grid.addWidget(self.__hpsdr, 3, 0)
-        self.__setStyle(self.__hpsdr)
+        hpsdr_label = QLabel("HPSDR")
+        hpsdr_cb_on = QCheckBox("On")
+        hpsdr_cb_off = QCheckBox("Off")
+        self.__hpsdr_grp = QButtonGroup()
+        self.__hpsdr_btn = QPushButton("Set")
+        self.__setup_func(grid, hpsdr_label, hpsdr_cb_on, hpsdr_cb_off, self.__hpsdr_grp, self.__hpsdr_btn, self.__hpsdr_evnt, 3)
         
-        self.__fcd = QPushButton("FunCube Dongle Plus")
-        self.__fcd.clicked.connect(self.__fcd_evnt)
-        grid.addWidget(self.__fcd, 4, 0)
-        self.__setStyle(self.__fcd)
+        fcd_label = QLabel("FunCube Dongle Plus")
+        fcd_cb_on = QCheckBox("On")
+        fcd_cb_off = QCheckBox("Off")
+        self.__fcd_grp = QButtonGroup()
+        self.__fcd_btn = QPushButton("Set")
+        self.__setup_func(grid, fcd_label, fcd_cb_on, fcd_cb_off, self.__fcd_grp, self.__fcd_btn, self.__fcd_evnt, 4)
     
-    def __setStyle(self, widget):
-        widget.setStyleSheet("QPushButton {color: rgb(148,148,148); font: bold 12px}")
+    #-------------------------------------------------
+    # Setup one function
+    def __setup_func(self, grid, label, cb_on, cb_off, group, button, evnt, col):
+        
+        grid.addWidget(label, col, 0)
+        grid.addWidget(cb_on, col, 1)
+        cb_off.setChecked(True)
+        grid.addWidget(cb_off, col, 2)
+        group.setExclusive(True)
+        group.addButton(cb_on)
+        group.setId(cb_on, 0)
+        group.addButton(cb_off)
+        group.setId(cb_off, 1)
+        button.clicked.connect(evnt)
+        grid.addWidget(button, col, 3)
+        self.__setStyle(label, cb_on, cb_off, button)
+        
+    #-------------------------------------------------
+    # Setup style  
+    def __setStyle(self, label, cb_on, cb_off, button):
+        label.setStyleSheet("QLabel {color: rgb(199,199,199); font: bold 12px}")
+        cb_on.setStyleSheet("QCheckBox {color: rgb(199,199,199); font: bold 12px}")
+        cb_off.setStyleSheet("QCheckBox {color: rgb(199,199,199); font: bold 12px}")
+        button.setStyleSheet("QPushButton {background-color: rgb(167,85,65); color: rgb(199,199,199); font: bold 12px}")
         
     #-------------------------------------------------
     # Event procs
